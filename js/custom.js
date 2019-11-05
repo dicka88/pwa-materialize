a7c7a8ee58fbef1b5b4c52d4059c6ee736622844
dicka88 <dickaismaji@gmail.com>
2019-11-06 05:53:35 +0700

    pages

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     var elems = document.querySelectorAll(".sidenav");
     M.Sidenav.init(elems);
     loadNav()
+    var page = window.location.hash.substr(1);
+    page = (page == "")? "home" : page
+    loadPage(page)
 })
 
 const loadNav = () => {
@@ -10,6 +13,17 @@ const loadNav = () => {
         url: 'nav.html',
         success: response => {
             $('.sidenav').html(response)
+        }
+    })
+}
+
+const loadPage = page => {
+    $.ajax({
+        method: 'get',
+        url: page+".html",
+        success: (response, text, xhr) => {
+            console.log(xhr.status)
+            $('#body-content').html(response)
         }
     })
 }
