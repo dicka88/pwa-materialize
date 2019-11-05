9215de5ff9e5ed5a041bb57e842f25b335f3534a
dicka88 <dickaismaji@gmail.com>
2019-11-06 05:57:19 +0700

    as

--- js/custom.js
+++ js/custom.js
@@ -27,26 +27,3 @@ const loadPage = page => {
         }
     })
 }
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Activate sidebar nav
-//     var elems = document.querySelectorAll(".sidenav");
-//     M.Sidenav.init(elems);
-//     loadNav();
-   
-//     function loadNav() {
-//       var xhttp = new XMLHttpRequest();
-//       xhttp.onreadystatechange = function() {
-//         if (this.readyState == 4) {
-//           if (this.status != 200) return;
-   
-//           // Muat daftar tautan menu
-//           document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
-//             elm.innerHTML = xhttp.responseText;
-//           });
-//         }
-//       };
-//       xhttp.open("GET", "nav.html", true);
-//       xhttp.send();
-//     }
-// }  )
