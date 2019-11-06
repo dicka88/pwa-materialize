9ec8d945e794a92114ec8553eca3c07c208c9241
dicka_ismaji <dicka_ismaji@quick.com>
2019-11-06 15:24:36 +0700

    With Api

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     var elems = document.querySelectorAll(".sidenav");
     M.Sidenav.init(elems);
     loadNav()
-    var page = window.location.hash.substr(1);
-    page = (page == "")? "home" : page
+    var page = window.location.hash.substr(1)
+    if(page == ''){
+        page = "pages/home.html"
+    }else{
+        page = "pages/"+page+".html"
+    }
+
+    console.log(page);
+    
     loadPage(page)
 })
 
@@ -24,7 +31,7 @@ $(document).ready(function(){
             M.Sidenav.getInstance(sidenav).close()
 
             page = $(this).attr('href').substr(1)
-            loadPage(page)
+            loadPage("pages/"+page+".html")
         })
     })
 })
@@ -34,7 +41,7 @@ const content = $('#body-content')
 const loadPage = page => {
     $.ajax({
         method: 'get',
-        url: "pages/"+page+".html",
+        url: page,
         success: (response, text, xhr) => {
             
             const code = xhr.status
@@ -60,6 +67,53 @@ const loadPage = page => {
                     break
                 default:
                     content.html("<p>URL not valid</p>")
+            }
+        } 
+    })
+}
+
+const loadApi = (target, link) => {
+    link = 'http://192.168.168.189/api/v1/'+link
+    $.ajax({
+        method: target,
+        url: link,
+        success: (response, text, xhr) => {
+            
+            const code = xhr.status
+            
+            switch(code){
+                case 200:
+                    let parsing = JSON.parse(response)
+                    console.log(parsing);
+                    
+                    var table = '<table>'
+                    parsing.forEach((item, id)=>{
+                        table += `<tr>
+                                <td>${item.id}</td>
+                                <td>${item.name}</td>
+                                <td>${item.teknik}</td>          
+                        </tr>`
+                    })
+                    
+                    content.html(table)
+                    break
+                case 404:
+                    content.html("<p>404 Error not found</p>")
+                    break
+                default:
+                    content.html("<p>URL not valid</p>")
+            }
+        },
+        error: function(log){
+            switch(log.status){
+                case 200:
+                    content.html(response)
+                    break
+                case 404:
+                    content.html("<p>404 Error not found</p>")
+                    break
+                default:
+                    content.html("<p>Check Network Connection</p>")
             }
         } 
     })
