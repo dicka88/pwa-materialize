7715bd54aca14b62059060a0f87b72572f891f35
dicka_ismaji <dicka_ismaji@quick.com>
2019-11-06 10:52:44 +0700

    Pwa with service worker

--- js/custom.js
+++ js/custom.js
@@ -17,13 +17,50 @@ const loadNav = () => {
     })
 }
 
+$(document).ready(function(){
+    $('.sidenav li a').each(function(elm){
+        this.addEventListener('click', function(event){
+            let sidenav = $('.sidenav')
+            M.Sidenav.getInstance(sidenav).close()
+
+            page = $(this).attr('href').substr(1)
+            loadPage(page)
+        })
+    })
+})
+
+const content = $('#body-content')
+
 const loadPage = page => {
     $.ajax({
         method: 'get',
-        url: page+".html",
+        url: "pages/"+page+".html",
         success: (response, text, xhr) => {
-            console.log(xhr.status)
-            $('#body-content').html(response)
-        }
+            
+            const code = xhr.status
+            
+            switch(code){
+                case 200:
+                    content.html(response)
+                    break
+                case 404:
+                    content.html("<p>404 Error not found</p>")
+                    break
+                default:
+                    content.html("<p>URL not valid</p>")
+            }
+        },
+        error: function(log){
+            switch(log.status){
+                case 200:
+                    content.html(response)
+                    break
+                case 404:
+                    content.html("<p>404 Error not found</p>")
+                    break
+                default:
+                    content.html("<p>URL not valid</p>")
+            }
+        } 
     })
 }
