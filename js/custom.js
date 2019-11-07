d42df1b842d38fa5676e4f9a868d583c48fd5bea
dicka_ismaji <dicka_ismaji@quick.com>
2019-11-07 09:40:50 +0700

    fix navbar

--- js/custom.js
+++ js/custom.js
@@ -1,120 +1,130 @@
 document.addEventListener("DOMContentLoaded", () => {
     var elems = document.querySelectorAll(".sidenav");
     M.Sidenav.init(elems);
-    loadNav()
-    var page = window.location.hash.substr(1)
-    if(page == ''){
-        page = "pages/home.html"
+    loadNav();
+    var page = window.location.hash.substr(1);
+    if(page === ''){
+        page = "pages/home.html";
     }else{
-        page = "pages/"+page+".html"
+        page = "pages/"+page+".html";
     }
 
     console.log(page);
     
-    loadPage(page)
-})
+    loadPage(page);
+});
+
+function route(elem){
+    let page = elem.attr('href').substr(1);
+    loadPage("pages/"+page+".html");
+    M.Sidenav.getInstance($('.sidenav')).close();
+}
 
 const loadNav = () => {
     $.ajax({
         method: 'get',
         url: 'nav.html',
         success: response => {
-            $('.sidenav').html(response)
+            $('.sidenav').html(response);
         }
-    })
-}
+    });
+};
 
 $(document).ready(function(){
-    $('.sidenav li a').each(function(elm){
-        this.addEventListener('click', function(event){
-            let sidenav = $('.sidenav')
-            M.Sidenav.getInstance(sidenav).close()
+    $('.sidenav > li > a').each(function(){
+        $(this).on('click', function(){
+            let sidenav = $('.sidenav');
+            M.Sidenav.getInstance(sidenav).close();
 
-            page = $(this).attr('href').substr(1)
-            loadPage("pages/"+page+".html")
-        })
-    })
-})
+            let page = $(this).attr('href').substr(1);
+            loadPage("pages/"+page+".html");
+        });
+    });
+});
 
-const content = $('#body-content')
+const content = $('#body-content');
 
-const loadPage = page => {
+function loadPage(page) {
     $.ajax({
         method: 'get',
         url: page,
         success: (response, text, xhr) => {
             
-            const code = xhr.status
+            const code = xhr.status;
             
             switch(code){
                 case 200:
-                    content.html(response)
-                    break
+                    content.html(response);
+                    break;
                 case 404:
-                    content.html("<p>404 Error not found</p>")
-                    break
+                    content.html("<p>404 Error not found</p>");
+                    break;
                 default:
-                    content.html("<p>URL not valid</p>")
+                    content.html("<p>URL not valid</p>");
             }
         },
         error: function(log){
             switch(log.status){
                 case 200:
-                    content.html(response)
-                    break
+                    content.html(response);
+                    break;
                 case 404:
-                    content.html("<p>404 Error not found</p>")
-                    break
+                    content.html("<p>404 Error not found</p>");
+                    break;
                 default:
-                    content.html("<p>URL not valid</p>")
+                    content.html("<p>URL not valid</p>");
             }
         } 
-    })
+    });
 }
 
 const loadApi = (target, link) => {
-    link = 'http://192.168.168.189/api/v1/'+link
+    link = 'https://api.kangkode.site/v1/'+link;
     $.ajax({
         method: target,
         url: link,
+        beforeSend: function(){
+          content.html("wait...");  
+        },
         success: (response, text, xhr) => {
             
-            const code = xhr.status
+            const code = xhr.status;
             
             switch(code){
                 case 200:
-                    let parsing = JSON.parse(response)
+                    let parsing = JSON.parse(response);
                     console.log(parsing);
                     
-                    var table = '<table>'
+                    var table = '<table>';
                     parsing.forEach((item, id)=>{
                         table += `<tr>
                                 <td>${item.id}</td>
                                 <td>${item.name}</td>
                                 <td>${item.teknik}</td>          
-                        </tr>`
-                    })
+                        </tr>`;
+                    });
                     
-                    content.html(table)
-                    break
+                    content.html(table);
+                    break;
                 case 404:
-                    content.html("<p>404 Error not found</p>")
-                    break
+                    content.html("<p>404 Error not found</p>");
+                    break;
                 default:
-                    content.html("<p>URL not valid</p>")
+                    content.html("<p>URL not valid</p>");
             }
         },
         error: function(log){
             switch(log.status){
                 case 200:
-                    content.html(response)
-                    break
+                    content.html(response);
+                    break;
                 case 404:
-                    content.html("<p>404 Error not found</p>")
-                    break
+                    content.html("<p>404 Error not found</p>");
+                    break;
                 default:
-                    content.html("<p>Check Network Connection</p>")
+                    content.html("<p>Check Network Connection</p>");
             }
         } 
-    })
-}
+    });
+    M.Sidenav.getInstance($('.sidenav')).close();
+};
